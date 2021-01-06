815e6356f7157902e95609f0b974b38aa244b3b5
Ryo Okuwaki <rokuwaki@gmail.com>
2021-01-06 00:46:35 +0000

    add screenshot

--- pyP.py
+++ pyP.py
@@ -25,7 +25,7 @@ class Index(object):
         fig.canvas.draw()
 
     def nextFurther(self, event):
-        self.ind += worknumtrace
+        self.ind += worknumtrace - 1
         if self.ind > totalnumtrace-1:
             self.ind = self.ind - totalnumtrace
         for ax in axs:
@@ -57,7 +57,7 @@ class Index(object):
         fig.canvas.draw()
 
     def prevFurther(self, event):
-        self.ind -= worknumtrace
+        self.ind -= worknumtrace - 1
         if self.ind < 0:
             self.ind = self.ind + totalnumtrace
         for ax in axs:
@@ -91,7 +91,7 @@ class Index(object):
 
     def nextFurtherKey(self, event):
         if event.key == 'right':
-            self.ind += worknumtrace
+            self.ind += worknumtrace - 1
             if self.ind > totalnumtrace-1:
                 self.ind = self.ind - totalnumtrace
             for ax in axs:
@@ -125,7 +125,7 @@ class Index(object):
 
     def prevFurtherKey(self, event):
         if event.key == 'left':
-            self.ind -= worknumtrace
+            self.ind -= worknumtrace - 1
             if self.ind < 0:
                 self.ind = self.ind + totalnumtrace
             for ax in axs:
@@ -163,11 +163,11 @@ def oncpick(event):
 
 def keypress(event):
     '''
-    press ",": Enlarge x range
-    press ".": Shrink x range
-    press "a": Enlarge y range
-    press "z": Shrink y range
-    press "x": Reset xrange
+    press ",": zoom in ylim
+    press ".": zoom out ylim
+    press "a": zoom in xlim
+    press "z": zoom out xlim
+    press "x": Reset xlim
     '''
     for ax in axs:
         x = ax.lines[0].get_xdata()
@@ -199,24 +199,17 @@ def keypress(event):
         ax.texts[0].set_y(ymax-(ymax-ymin)*0.1)
     fig.canvas.draw()
 
-def aziequi(ax, staloc0, staloc1):
-    #data = np.loadtxt(stalist, usecols=(5, 4))
-    #d, a = (data[:, 0], 90-data[:,1])
-    x, y = staloc0, staloc1
+def aziequi(ax, azilist, dellist):
+    d, a = np.array(dellist), 90-np.array(azilist)
+    x, y=(d*np.cos(a*np.pi/180.0), d*np.sin(a*np.pi/180.0))
     sc=ax.scatter(x, y, s=15, marker='^', edgecolor='none', facecolor='gray', alpha=0.85, zorder=10)
     sc=ax.scatter(x, y, s=15, marker='^', edgecolor='k', facecolor='none', alpha=1, lw=0.5, zorder=10)
-    #stalist=np.loadtxt(stalist, usecols=(0), dtype=str)
-    #print(stalist)
-    #for (i, staname) in enumerate(stalist):
-        #text = ax.text(x[i], y[i], staname, size=10, va='center')
-        #text.set_path_effects([path_effects.Stroke(linewidth=1, foreground='w', alpha=0.85), path_effects.Normal()])
-    #ax.scatter(0, 0, s=100, marker='*', edgecolor='k', facecolor='none')
     theta=np.linspace(0, 360, 360)
     for i in [30, 60, 90]:
         x, y=(i*np.cos(theta*np.pi/180.0), i*np.sin(theta*np.pi/180.0))
         ax.plot(x, y, color='k', zorder=0, solid_capstyle='round', lw=0.1)
         x, y=(i*np.cos(-90*np.pi/180.0), i*np.sin(-90*np.pi/180.0))
-        text = ax.text(x, y, str(i)+'$\degree$', size=6, va='center', ha='center')
+        text = ax.text(x, y, str(i)+'$\degree$', size=8, va='center', ha='center')
         text.set_path_effects([path_effects.Stroke(linewidth=2, foreground='w', alpha=1), path_effects.Normal()])
     delrange = np.linspace(0, 100, 10)
     for i in np.arange(0, 360, 30):
@@ -237,29 +230,29 @@ def aziequi(ax, staloc0, staloc1):
     ax.set_yticks([])
     return sc
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Button, MultiCursor
-from matplotlib.gridspec import GridSpec
 import sys
 import glob
 import argparse
-from obspy import read
 import mplcursors
-import matplotlib.patheffects as path_effects
-from datetime import datetime
+import numpy as np
+from obspy import read
 import matplotlib as mpl
 from cycler import cycler
+from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from geographiclib.geodesic import Geodesic
+import matplotlib.patheffects as path_effects
+from matplotlib.widgets import Button, MultiCursor
 cmap = plt.get_cmap('Set3', 12)
 custom_color_cycle = [ str(mpl.colors.rgb2hex(cmap(i)[:3])) for i in range(cmap.N) ]
 plt.rc('axes', prop_cycle=(cycler(color=custom_color_cycle)))
-from geographiclib.geodesic import Geodesic
 geod = Geodesic.WGS84
-
 textpe = [path_effects.Stroke(linewidth=2, foreground='w', alpha=1), path_effects.Normal()]
 xmin0 = -100
 xmax0 = 400
 
+# Argument check
 parser = argparse.ArgumentParser(description='pyP: Python based P-arrival picking tool')
 parser.add_argument('arg1', help='Number of traces shown in display (e.g., 7)')
 parser.add_argument('arg2', help='SAC files you want to pick P arrival (e.g., "./*.SAC") *Do not forget quotation marks!')
@@ -284,6 +277,7 @@ if len(glob.glob(sacfiles)) == 0:
     print('')
     sys.exit(1)
 
+# Load SAC files and sort traces based on station azimuth
 st = read(sacfiles)
 elat, elon = float(args.arg3), float(args.arg4) #38.392, 39.085
 for j in range(len(st)):
@@ -295,17 +289,15 @@ for j in range(len(st)):
     trace.stats.sac.user0 = azi
     trace.stats.sac.user1 = tmp['a12']
     st[j] = trace
-st.traces.sort(key=lambda x: x.stats.sac.user0)
+st.traces.sort(key=lambda x: x.stats.sac.user0) # sort by azimuth
 
+# Base subplots, which is not shown
 totalnumtrace = len(glob.glob(sacfiles))
-#worknumtrace = 8
-amarkerlist = np.zeros(totalnumtrace)
-
 fig, axs = plt.subplots(totalnumtrace, figsize=(15, 10))
 fig.subplots_adjust(left=0.3)
 
-azilist = []
-dellist = []
+amarkerlist = np.zeros(totalnumtrace)
+azilist, dellist = [], []
 for j,ax in enumerate(axs.flat):
     trace = st[j].copy()
     amarker = trace.stats.sac.a
@@ -324,7 +316,6 @@ for j,ax in enumerate(axs.flat):
     ax.set_ylim(ymin0, ymax0)
     textlabel = str(j+1)+'/'+str(totalnumtrace)+'\n'+str(trace.stats.network)+'.'+str(trace.stats.station)+'.'+str(trace.stats.location)+'.'+str(trace.stats.channel)
     textlabel1 = '\nAzi: '+str('{:.2f}'.format(trace.stats.sac.user0)) + '\nDel: ' + str('{:.2f}'.format(trace.stats.sac.user1))
-    #axp = ax.get_position()
     text = ax.text(xmin0+(xmax0-xmin0)*0.005, ymax0-(ymax0-ymin0)*0.1, textlabel+textlabel1, fontsize=8, ha='left', va='top').set_path_effects(textpe)
 
     ax.patch.set_facecolor('C'+str(j))
@@ -332,15 +323,12 @@ for j,ax in enumerate(axs.flat):
     ax.tick_params(labelleft=False)
     ax.tick_params(labelbottom=False)
 
+
+# Display canvus / Interactive click actions
 gs = GridSpec(worknumtrace,1, hspace=0)
-
-
-
 callback = Index()
 
 axp = axs[-1].get_position()
-
-#axp.x1-axp.width*0.1, axp.y1+0.01, axp.width*0.1, axp.height*0.75
 tmpw = axp.width*0.08
 axprevFurther = plt.axes([axp.x0, axp.y0-0.05, tmpw, 0.035])
 axprev = plt.axes([axp.x0+tmpw+0.02, axp.y0-0.05, tmpw, 0.035])
@@ -361,15 +349,15 @@ bnextFurther.on_clicked(callback.nextFurther)
 
 kpnext = fig.canvas.mpl_connect('key_press_event', callback.nextKey)
 kpnextFurther = fig.canvas.mpl_connect('key_press_event', callback.nextFurtherKey)
+
 kpprev = fig.canvas.mpl_connect('key_press_event', callback.prevKey)
 kpprevFurther = fig.canvas.mpl_connect('key_press_event', callback.prevFurtherKey)
 
 curser = MultiCursor(fig.canvas, axs, color='#B12763', lw=1, alpha=0.75)
-
 fig.canvas.mpl_connect('button_press_event', oncpick)
 fig.canvas.mpl_connect('key_press_event', keypress)
 
-
+# Initial setting: hide base subplots and display first `worknumtrace` subplots
 for ax in axs:
     ax.set_visible(False)
 
@@ -377,20 +365,22 @@ for j in np.arange(0, worknumtrace, 1):
     axs[j].set_visible(True)
     axs[j].set_position(gs[j].get_position(fig))
 
+# Show usage at top
+axp = axs[0].get_position()
+usagetext = 'Usage: press key\n[A] Zoom-in xlim      [Z] Zoom-out xlim      [X] Rest xlim\n[.] Zoom-in ylim        [,] Zoom-out ylim\n'+\
+r'[$\downarrow$] Next      [$\rightarrow$] Next more      [$\uparrow$] Prev      [$\leftarrow$] Prev more'
+fig.text(axp.x0, axp.y1+0.01, usagetext, size=8)
+
+# Station map
 axp = axs[-1].get_position()
-axbutton = fig.add_axes([axp.x1-0.1, axp.y0-0.05, 0.1, 0.035])
 axstamap = fig.add_axes([axp.x0-0.255, axp.y0, 0.25, 0.25])
 axstamap.set_aspect(1)
+sc = aziequi(axstamap, azilist, dellist)
 
+# Save function
+axbutton = fig.add_axes([axp.x1-0.1, axp.y0-0.05, 0.1, 0.035])
 axp = axbutton.get_position()
 fig.text(axp.x0-0.01, axp.y0+axp.height/2, '', ha='right', va='center')
 bsave = Button(axbutton, 'Save')
-
-#axp=ax.get_position()
-d, a = np.array(dellist), 90-np.array(azilist)
-staloc0, staloc1=(d*np.cos(a*np.pi/180.0), d*np.sin(a*np.pi/180.0))
-sc = aziequi(axstamap, staloc0, staloc1)
-
-
 
 plt.show()
