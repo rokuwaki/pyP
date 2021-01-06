b52b12a4243c31012e4ddcccf29b2557b0f9b5f9
Ryo Okuwaki <rokuwaki@gmail.com>
2021-01-06 21:51:01 +0000

    add screenshot

--- pyP.py
+++ pyP.py
@@ -192,7 +192,7 @@ def checkArgparse():
     parser = argparse.ArgumentParser(description='pyP: Python based P-arrival picking tool',
                                     usage='test.py [-h] displayNum sacfiles elat elon'+\
                                     '\n\nExample:'+\
-                                    '\n>>> python pyP.py 7 "../__bak/pyPick/SACfiles/II*.SAC,../__bak/pyPick/SACfiles/II.*.SAC" 36.11 140.10\n ')
+                                    '\n>>> python pyP.py 7 "../II*.SAC,../IU.*.SAC" 36.11 140.10\n ')
     parser.add_argument('displayNum', help='Number of traces shown in display (e.g., 7)', type=int)
     parser.add_argument('sacfiles', help='SAC files you want to pick P arrival (e.g., "./*.SAC"). comma-separated list is available. *Do not forget quotation marks!', type=str)
     parser.add_argument('elat', help='Latitude of epicentre (for station azimuth)', type=float)
